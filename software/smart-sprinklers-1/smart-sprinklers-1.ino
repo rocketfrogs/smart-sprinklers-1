35b45e23784dc5b73cb7c08ceb0d7a162e459545
arthur-descamps <arthur.descamps@club.fr>
2020-05-09 18:45:57 -0700

    Cedric code update - tested, working like versaille !

--- software/smart-sprinklers-1/smart-sprinklers-1.ino
+++ software/smart-sprinklers-1/smart-sprinklers-1.ino
@@ -14,6 +14,21 @@
 
 #define DEVICE_NAME "smart-sprinkler-1"
 
+#define PIN_SOURCE   25
+#define PIN_CHANNEL1 26
+#define PIN_CHANNEL2 27
+#define PIN_CHANNEL3 14
+#define PIN_CHANNEL4 13
+#define PIN_CHANNEL5 21
+#define PIN_CHANNEL6 22
+#define PIN_CHANNEL7 23
+
+#define SPRINKLER_ON_LEVEL HIGH
+#define SPRINKLER_OFF_LEVEL LOW
+
+#define SOURCE_ESP_LEVEL LOW
+#define SOURCE_RAINBIRD_LEVEL HIGH
+
 /*
   Content of "credentials.h" that matters for this section
 
@@ -45,31 +60,45 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 
 // Setup feeds for publishing.
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish stat_channel1 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel1");
-Adafruit_MQTT_Publish stat_channel2 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel2");
-Adafruit_MQTT_Publish stat_channel3 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel3");
-Adafruit_MQTT_Publish stat_channel4 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel4");
-Adafruit_MQTT_Publish stat_channel5 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel5");
-Adafruit_MQTT_Publish stat_channel6 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel6");
-Adafruit_MQTT_Publish stat_channel7 = Adafruit_MQTT_Publish(&mqtt, DEVICE_NAME "/stat/channel7");
+Adafruit_MQTT_Publish stat_source = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/source");
+Adafruit_MQTT_Publish stat_channel1 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel1");
+Adafruit_MQTT_Publish stat_channel2 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel2");
+Adafruit_MQTT_Publish stat_channel3 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel3");
+Adafruit_MQTT_Publish stat_channel4 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel4");
+Adafruit_MQTT_Publish stat_channel5 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel5");
+Adafruit_MQTT_Publish stat_channel6 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel6");
+Adafruit_MQTT_Publish stat_channel7 = Adafruit_MQTT_Publish(&mqtt, "/" DEVICE_NAME "/stat/channel7");
 
 // Setup a feeds for subscribing to changes.
-Adafruit_MQTT_Subscribe cmnd_channel1 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel1");
-Adafruit_MQTT_Subscribe cmnd_channel2 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel2");
-Adafruit_MQTT_Subscribe cmnd_channel3 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel3");
-Adafruit_MQTT_Subscribe cmnd_channel4 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel4");
-Adafruit_MQTT_Subscribe cmnd_channel5 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel5");
-Adafruit_MQTT_Subscribe cmnd_channel6 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel6");
-Adafruit_MQTT_Subscribe cmnd_channel7 = Adafruit_MQTT_Subscribe(&mqtt, DEVICE_NAME "/cmnd/channel7");
+Adafruit_MQTT_Subscribe cmnd_source = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/source");
+Adafruit_MQTT_Subscribe cmnd_channel1 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel1");
+Adafruit_MQTT_Subscribe cmnd_channel2 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel2");
+Adafruit_MQTT_Subscribe cmnd_channel3 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel3");
+Adafruit_MQTT_Subscribe cmnd_channel4 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel4");
+Adafruit_MQTT_Subscribe cmnd_channel5 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel5");
+Adafruit_MQTT_Subscribe cmnd_channel6 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel6");
+Adafruit_MQTT_Subscribe cmnd_channel7 = Adafruit_MQTT_Subscribe(&mqtt, "/" DEVICE_NAME "/cmnd/channel7");
 
-static void set_channel_1(uint32_t enable);
-static void set_channel_2(uint32_t enable);
-static void set_channel_3(uint32_t enable);
-static void set_channel_4(uint32_t enable);
-static void set_channel_5(uint32_t enable);
-static void set_channel_6(uint32_t enable);
-static void set_channel_7(uint32_t enable);
-static void set_channel_8(uint32_t enable);
+enum {
+  SOURCE_RAINBIRD,
+  SOURCE_ESP
+} source;
+
+#define CHANNEL_COUNT 7
+struct {
+  bool enabled; // is the channel currently enabled?
+  Adafruit_MQTT_Publish *stat; // stats
+  Adafruit_MQTT_Subscribe *cmnd; // command
+  int gpio; // which gpio the enable signal is connected to
+} channels[CHANNEL_COUNT] = {
+  {false, &stat_channel1, &cmnd_channel1, PIN_CHANNEL1},
+  {false, &stat_channel2, &cmnd_channel2, PIN_CHANNEL2},
+  {false, &stat_channel3, &cmnd_channel3, PIN_CHANNEL3},
+  {false, &stat_channel4, &cmnd_channel4, PIN_CHANNEL4},
+  {false, &stat_channel5, &cmnd_channel5, PIN_CHANNEL5},
+  {false, &stat_channel6, &cmnd_channel6, PIN_CHANNEL6},
+  {false, &stat_channel7, &cmnd_channel7, PIN_CHANNEL7}
+};
 
 ///////////////////// initialize variables  ///////////////////////////
 
@@ -142,16 +171,9 @@ void setup() {
   ///////////////////////////////////////////////////////
   ////////////// Suscribing to MQTT topics //////////////
   ///////////////////////////////////////////////////////
-
-  cmnd_channel1.setCallback(set_channel_1);
-  cmnd_channel2.setCallback(set_channel_2);
-  cmnd_channel3.setCallback(set_channel_3);
-  cmnd_channel4.setCallback(set_channel_4);
-  cmnd_channel5.setCallback(set_channel_5);
-  cmnd_channel6.setCallback(set_channel_6);
-  cmnd_channel7.setCallback(set_channel_7);
   
   // Setup MQTT subscription feeds.
+  mqtt.subscribe(&cmnd_source);
   mqtt.subscribe(&cmnd_channel1);
   mqtt.subscribe(&cmnd_channel2);
   mqtt.subscribe(&cmnd_channel3);
@@ -164,39 +186,17 @@ void setup() {
   ////////////////// Initialize PINs ////////////////////
   ///////////////////////////////////////////////////////
 
-#define PIN_CHANNEL1_SRC 0
-#define PIN_CHANNEL2_SRC 0
-#define PIN_CHANNEL3_SRC 0
-#define PIN_CHANNEL4_SRC 0
-#define PIN_CHANNEL5_SRC 0
-#define PIN_CHANNEL6_SRC 0
-#define PIN_CHANNEL7_SRC 0
+  // Open drain outputs (either input or low output)
+  source = SOURCE_RAINBIRD;
+  openDrainWrite(PIN_SOURCE, SOURCE_RAINBIRD_LEVEL);
+  stat_source.publish(SOURCE_RAINBIRD);
+  for (int i = 0; i < CHANNEL_COUNT; i++) {
+    openDrainWrite(channels[i].gpio, SPRINKLER_OFF_LEVEL);
+    channels[i].stat->publish(0);
+  }
 
-#define PIN_CHANNEL1_ENABLE 0
-#define PIN_CHANNEL2_ENABLE 0
-#define PIN_CHANNEL3_ENABLE 0
-#define PIN_CHANNEL4_ENABLE 0
-#define PIN_CHANNEL5_ENABLE 0
-#define PIN_CHANNEL6_ENABLE 0
-#define PIN_CHANNEL7_ENABLE 0
-
-  // OUTPUTS
-  pinMode(PIN_CHANNEL1_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL2_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL3_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL4_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL5_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL6_SRC, OUTPUT);
-  pinMode(PIN_CHANNEL7_SRC, OUTPUT);
-
-  pinMode(PIN_CHANNEL1_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL2_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL3_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL4_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL5_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL6_ENABLE, OUTPUT);
-  pinMode(PIN_CHANNEL7_ENABLE, OUTPUT);
 }
+
 ///////////////////////////////////////////////////////
 ///  //////////////////////////////////////////////////
 ///  //////////////////////////////////////////////////
@@ -228,7 +228,33 @@ void loop() {
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
   // function definition further below.
   MQTT_connect();
-  mqtt.processPackets(10);
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(250))) {
+    int value = atoi((char*)subscription->lastread);
+    if (subscription == &cmnd_source) {
+      if (value == SOURCE_RAINBIRD) {
+        source = SOURCE_RAINBIRD;
+        openDrainWrite(PIN_SOURCE, SOURCE_RAINBIRD_LEVEL);
+      } else if (value == SOURCE_ESP) {
+        source = SOURCE_ESP;
+        openDrainWrite(PIN_SOURCE, SOURCE_ESP_LEVEL);
+      }
+      stat_source.publish(source);
+    } else {
+      for (int i = 0; i < CHANNEL_COUNT; i++) {
+        if (subscription == channels[i].cmnd) {
+          bool enable = (value != 0);
+          openDrainWrite(channels[i].gpio, enable ? SPRINKLER_ON_LEVEL : SPRINKLER_OFF_LEVEL);
+          channels[i].enabled = enable;
+          channels[i].stat->publish(enable);
+          break;
+        }
+      }
+    }
+  }
+
+  print_stats();
 }
 
 ///////////////////////////////////////////////////////
@@ -255,6 +281,23 @@ void loop() {
 /////\  ///////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 
+void openDrainWrite(int pin, int level) {
+  if (level) {
+    pinMode(pin, INPUT);
+  } else {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+}
+
+void print_stats() {
+  unsigned long now = millis();
+  static unsigned long prev = 0;
+  if (now - prev > 1000) {
+    Serial.println("älive!");
+    prev = now;
+  }
+}
 
 ///////////////////////////////////////////////////////
 //////////////// MQTT_connect Function ////////////////
@@ -270,16 +313,12 @@ void MQTT_connect() {
     return;
   }
 
-#ifdef ENABLE_SERIAL
   Serial.print("Connecting to MQTT... ");
-#endif
 
   uint8_t retries = 3;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-#ifdef ENABLE_SERIAL
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
-#endif
     mqtt.disconnect();
     delay(250);  // wait 5 seconds
     retries--;
@@ -288,144 +327,13 @@ void MQTT_connect() {
       while (1);
     }
   }
-#ifdef ENABLE_SERIAL
   Serial.println("MQTT Connected!");
-#endif
-}
 
-
-
-static void set_channel_1(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL1_SRC, LOW);
-    digitalWrite(PIN_CHANNEL1_ENABLE, LOW);
-    stat_channel1.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL1_SRC, LOW);
-    digitalWrite(PIN_CHANNEL1_ENABLE, LOW);
-    stat_channel1.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL1_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL1_ENABLE, LOW);
-    stat_channel1.publish(-1);
+  stat_source.publish(source);
+  for (int i = 0; i < CHANNEL_COUNT; i++) {
+    channels[i].stat->publish(channels[i].enabled);
   }
-}
 
-static void set_channel_2(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL2_SRC, LOW);
-    digitalWrite(PIN_CHANNEL2_ENABLE, LOW);
-    stat_channel2.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL2_SRC, LOW);
-    digitalWrite(PIN_CHANNEL2_ENABLE, LOW);
-    stat_channel2.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL2_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL2_ENABLE, LOW);
-    stat_channel2.publish(-1);
-  }
-}
-
-static void set_channel_3(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL3_SRC, LOW);
-    digitalWrite(PIN_CHANNEL3_ENABLE, LOW);
-    stat_channel3.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL3_SRC, LOW);
-    digitalWrite(PIN_CHANNEL3_ENABLE, LOW);
-    stat_channel3.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL3_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL3_ENABLE, LOW);
-    stat_channel3.publish(-1);
-  }
-}
-
-static void set_channel_4(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL4_SRC, LOW);
-    digitalWrite(PIN_CHANNEL4_ENABLE, LOW);
-    stat_channel4.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL4_SRC, LOW);
-    digitalWrite(PIN_CHANNEL4_ENABLE, LOW);
-    stat_channel4.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL4_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL4_ENABLE, LOW);
-    stat_channel4.publish(-1);
-  }
-}
-
-static void set_channel_5(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL5_SRC, LOW);
-    digitalWrite(PIN_CHANNEL5_ENABLE, LOW);
-    stat_channel5.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL5_SRC, LOW);
-    digitalWrite(PIN_CHANNEL5_ENABLE, LOW);
-    stat_channel5.publish(1);
-  } else {
-    // Original programme
-    digitalWrite(PIN_CHANNEL5_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL5_ENABLE, LOW);
-    stat_channel5.publish(-1);
-  }
-}
-
-static void set_channel_6(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL6_SRC, LOW);
-    digitalWrite(PIN_CHANNEL6_ENABLE, LOW);
-    stat_channel6.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL6_SRC, LOW);
-    digitalWrite(PIN_CHANNEL6_ENABLE, LOW);
-    stat_channel6.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL6_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL6_ENABLE, LOW);
-    stat_channel6.publish(-1);
-  }
-}
-
-static void set_channel_7(uint32_t enable) {
-  if (enable == 0) {
-    // OFF
-    digitalWrite(PIN_CHANNEL7_SRC, LOW);
-    digitalWrite(PIN_CHANNEL7_ENABLE, LOW);
-    stat_channel7.publish(0);
-  } else if (enable == 1) {
-    // ON
-    digitalWrite(PIN_CHANNEL7_SRC, LOW);
-    digitalWrite(PIN_CHANNEL7_ENABLE, LOW);
-    stat_channel7.publish(1);
-  } else {
-    // Original programmer
-    digitalWrite(PIN_CHANNEL7_SRC, HIGH);
-    digitalWrite(PIN_CHANNEL7_ENABLE, LOW);
-    stat_channel7.publish(-1);
-  }
 }
 
 ///////////////////////////////////////////////////////
